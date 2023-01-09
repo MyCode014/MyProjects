6db53bcf7b8190f0f6e20413f15e3f442a294c1d
MyCode014 <122278978+MyCode014@users.noreply.github.com>
2023-01-09 11:20:47 -0800

    Add files via upload

---
+++ ShoppingCart/src/data.js
@@ -0,0 +1,28 @@
+
+import tel3 from './images/tel3.png';
+import tel5 from './images/tel5.png';
+import tel6 from './images/tel6.png';
+
+export default [
+  {
+    id: 1,
+    title: 'iPhone 12vmini',
+    price: 599.99,
+    img:  tel3,
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'iPhone 12 Pro ',
+    price: 499.99,
+    img: tel5,
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'iPhone 12 Pro max',
+    price: 699.99,
+    img: tel6,
+    amount: 1,
+  },
+];
